bb236c931f96d02a3d1f95fda8ae89a11ccaac46
Karen Marting <karenannemarting@yahoo.com>
2021-07-17 18:23:08 -0700

    changed path to main imgae

--- script.js
+++
@@ -1,8 +0,0 @@
-// function viewAboutMe() {
-//     document.getElementById('bio').setAttribute('style', 'display: block');
-//   }
-//   document.getElementById('closeBtn').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     this.parentNode.style.display = 'none';
-//   }, false);
-
